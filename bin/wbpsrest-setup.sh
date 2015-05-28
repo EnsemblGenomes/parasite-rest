07a95fba761d4e5a3172cead73ebaf38bc1bce34
Bruce Bolt <bbolt@ebi.ac.uk>
2015-05-28 10:53:57 +0100

    Fix filename error in setup script

--- bin/wbpsrest-setup.sh
+++ bin/wbpsrest-setup.sh
@@ -84,7 +84,7 @@ cp -v ../eg-rest/ensembl_rest.psgi ../ensembl-rest
 cp -rv ../eg-rest/root/static/* ../ensembl-rest/root/static/
 cp -rv root/static/* ../ensembl-rest/root/static/
 cp -v ./root/favicon.ico ../ensembl-rest/root/favicon.ico
-cp -v ./root/favicon.ico ../ensembl-rest/root/wrapper.tt
+cp -v ./root/root/wrapper.tt ../ensembl-rest/root/wrapper.tt
 
 ## Remove some some endpoints we dont want
 rm -v ../ensembl-rest/root/documentation/vep.conf
