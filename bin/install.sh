db9a3228cba3e87a188a8949e086698010600435
Tuan Le <tuanle@ebi.ac.uk>
2020-10-05 21:00:49 +0100

    Update the install script, working for E! release/98

--- bin/install.sh
+++ bin/install.sh
@@ -4,17 +4,19 @@ ver2=$2
 ver3=$3
 
 if [ $ver1 ]; then ENSEMBL_BRANCH="$ver1"; fi
-if [ $ver2 ]; then EG_BRANCH="$ver2"; fi
-if [ $ver3 ]; then WBPS_BRANCH="$ver3"; fi
+if [ $ver2 ]; then WBPS_BRANCH="$ver2"; fi
+if [ $ver3 ]; then REST_VERSION="$ver3"; fi
 
+eval `ssh-agent -s`
+ssh-add
 
 ## Check out *Ensembl* code (API, web and (web) tools) from GitHub:
 for repo in \
     ensembl \
     ensembl-compara \
     ensembl-funcgen \
-    ensembl-tools \
     ensembl-variation \
+    ensembl-metadata \
     ensembl-rest \
     ensembl-io;
 do
@@ -33,25 +35,8 @@ do
     echo
 done
 
-## Check out *Ensembl Genomes* code (API, web and (web) tools) from GitHub:
-for repo in \
-    ensemblgenomes-api \
-    eg-rest;
-do
-    if [ ! -d "$repo" ]; then
-        echo "Checking out $repo (branch ${EG_BRANCH})"
-        git clone --branch ${EG_BRANCH} https://github.com/EnsemblGenomes/${repo}
-    else
-        echo Already got $repo, attempting to pull...
-        cd $repo
-        git pull
-        git status
-        cd ../
-    fi
-
-    echo
-    echo
-done
+# Taxonomy
+git clone --branch master https://github.com/Ensembl/ensembl-taxonomy.git
 
 ## Check out the ParaSite code
 for repo in \
@@ -79,20 +64,26 @@ mkdir logs
 cd parasite-rest
 
 ## ParaSite Configuration
-cp -v ../eg-rest/ensembl_rest.psgi ../ensembl-rest
-cp -rv ../eg-rest/root/static/* ../ensembl-rest/root/static/
+cp -rv root/static/* ../ensembl-rest/root/static/
+cp -v ./root/favicon.ico ../ensembl-rest/root/favicon.ico
+cp -v ./root/wrapper.tt ../ensembl-rest/root/wrapper.tt
+cp -v ./root/documentation/index.tt ../ensembl-rest/root/documentation/index.tt
+cp -v ./root/documentation/info.tt ../ensembl-rest/root/documentation/info.tt
+cp -v ensembl_rest.psgi ../ensembl-rest/ensembl_rest.psgi 
+
+## Remove controller libs we dont want
+rm -rfv ../ensembl-rest/lib/EnsEMBL/REST/Controller/VEP.pm
+rm -rfv ../ensembl-rest/lib/EnsEMBL/REST/Controller/VariantRecoder.pm
+rm -rfv ../ensembl-rest/lib/EnsEMBL/REST/Controller/ga4gh
+rm -rfv ../ensembl-rest/lib/EnsEMBL/REST/Model/ga4gh
 
 ## Remove some some endpoints we dont want
-rm -rv ../ensembl-rest/lib/EnsEMBL/REST/Controller/ga4gh
-rm -rv ../ensembl-rest/lib/EnsEMBL/REST/Model/ga4gh
-rm -v ../ensembl-rest/root/documentation/examples/*
 rm -v ../ensembl-rest/root/documentation/vep.conf
 rm -v ../ensembl-rest/root/documentation/compara.conf
 rm -v ../ensembl-rest/root/documentation/overlap.conf
 rm -v ../ensembl-rest/root/documentation/regulatory.conf
 rm -v ../ensembl-rest/root/documentation/taxonomy.conf
 rm -v ../ensembl-rest/root/documentation/variation.conf
-rm -v ../ensembl-rest/root/documentation/gafeatures.conf
 rm -v ../ensembl-rest/root/documentation/gavariant.conf
 rm -v ../ensembl-rest/root/documentation/gavariantset.conf
 rm -v ../ensembl-rest/root/documentation/gacallset.conf
@@ -104,15 +95,7 @@ rm -v ../ensembl-rest/root/documentation/sequence.conf
 rm -v ../ensembl-rest/root/documentation/xrefs.conf
 rm -v ../ensembl-rest/root/documentation/archive.conf
 rm -v ../ensembl-rest/root/documentation/ld.conf
-rm -v ../ensembl-rest/root/documentation/transcripthaplotypes.conf
 rm -v ../ensembl-rest/root/documentation/phenotypefeatures.conf
-rm -v ../eg-rest/root/documentation/variation.conf
-rm -v ../eg-rest/root/documentation/vep.conf
-rm -v ../eg-rest/root/documentation/compara.conf
-rm -v ../eg-rest/root/documentation/info.conf
-rm -v ../eg-rest/root/documentation/overlap.conf
-rm -v ../eg-rest/root/documentation/sequence.conf
 
-## Compile the variation code
-(cd ensembl-variation/C_code && make)
-
+#Update Rest version to be Parasite's version
+sed -i "/\$VERSION/c our \$VERSION = $REST_VERSION;" ../ensembl-rest/lib/EnsEMBL/REST.pm
